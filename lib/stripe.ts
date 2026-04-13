0c4ae39e8e3b0237393d1633839d33347d3db31e
Khalid Bin Helaly <khalidbinhelaly@Khalids-MacBook-Pro.local>
2026-04-13 17:57:26 +0600

    Remove Stripe client from lib level

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,3 @@
-import Stripe from 'stripe'
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-03-25.dahlia',
-})
-
 export const PLANS = {
   starter: {
     name: 'Starter',
